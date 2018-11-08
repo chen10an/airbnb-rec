fd04b4b09b93644676f1a674548c2410a5436dee
Susie Choi <sc477@duke.edu>
2018-11-08 16:48:56 -0500

    appy into recommender

--- airbnbrec/airbnbrec/settings.py
+++ airbnbrec/airbnbrec/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'recommender.apps.RecommenderConfig'
+    'recommender',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- airbnbrec/recommender/views.py
+++ airbnbrec/recommender/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the recommender index.")
+def home(request):
+    return render(request, 'recommender/homePage.html')
