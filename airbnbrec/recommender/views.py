e63aec40fcbfbe8a19bf40fb96c4d4706d4b3384
AngC187 <ac496@duke.edu>
2018-11-08 21:38:27 -0500

    created dummy view

--- airbnbrec/recommender/views.py
+++ airbnbrec/recommender/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Listing, Offering
 
 def home(request):
     return render(request, 'recommender/homePage.html')
+
+def simple(request):
+	top_listings=Listing.objects.order_by('-id')[:5] #we can change this to order by match_weight later
+	context = {'top_listings': top_listings}
+	return render(request, 'recommender/simple.html', context) 
