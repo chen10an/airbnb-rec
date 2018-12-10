3053a10f08627bcfae67ec9a2d040f3cb7184c47
AngC187 <ac496@duke.edu>
2018-12-10 16:42:16 -0500

    working with Yelp data

--- airbnbrec/recommender/urls.py
+++ airbnbrec/recommender/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('top5/', views.simple),
-    #path('customizePage/', views.customize),
-    # path('topMatches/', views.match),
     path('customize', views.customize),
 	path('load', views.load),
 	path('listing', views.listing)
--- airbnbrec/recommender/views.py
+++ airbnbrec/recommender/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .models import Listing, Offering
+from .models import Listing, Offering, Business
 from datetime import datetime
 
 def home(request):
@@ -47,13 +47,6 @@ def load(request):
 		request.session['p3'] = request.POST['priority3']
 		request.session['p4'] = request.POST['priority4']
 		request.session['p5'] = request.POST['priority5']
-		# p1=request.POST['priority1']
-		# p2=request.POST['priority2']
-		# p3=request.POST['priority3']
-		# p4=request.POST['priority4']
-		# p5=request.POST['priority5']
-		# context={'p1':p1, 'p2':p2, 'p3':p3, 'p4':p4, 'p5':p5}
-		# return render(request, 'recommender/simpleMatches.html', context)
 
 	return render(request, 'recommender/spinnyBoi.html')
 
@@ -85,22 +78,100 @@ def listing(request):
 	p5 = request.session.get('p5')
 
 	#run the SQL query using these parameters and give the results to match page
-	# matches=Listing.objects.raw("SELECT DISTINCT id, listing_url FROM Listing AS l, Offering WHERE %s=neighborhood AND accommodates>=%s AND guests_included<=%s AND %s=(SELECT COUNT(*) FROM Offering WHERE date_for_stay<=%s AND date_for_stay>=%s AND available='t' AND listing_id=l.id) AND %s::MONEY<=(((%s-guests_included)*extra_people*%s)::MONEY + (SELECT SUM(price) FROM Offering WHERE date_for_stay<=%s AND date_for_stay>=%s AND listing_id=l.id)) AND %s::MONEY>=(((%s-guests_included)*extra_people*%s)::MONEY + (SELECT SUM(price) FROM Offering WHERE date_for_stay<=%s AND date_for_stay>=%s AND listing_id=l.id)) AND %s>=minimum_nights AND %s<=maximum_nights AND %s<=bedrooms AND %s<=beds", [neighborhood, numGuests, numGuests, nights, checkout, checkin, minPrice, numGuests, nights, checkout, checkin, maxPrice, numGuests, nights, checkout, checkin, nights, nights, numRooms, numBeds])
-	# context = {'matches': matches}
 	matches=Listing.objects.raw("SELECT DISTINCT id, listing_url FROM Listing AS l, Offering WHERE %s=neighborhood AND accommodates>=%s AND guests_included<=%s AND %s=(SELECT COUNT(*) FROM Offering WHERE date_for_stay<=%s AND date_for_stay>=%s AND available='t' AND listing_id=l.id) AND %s::MONEY<=(((%s-guests_included)*extra_people*%s)::MONEY + (SELECT SUM(price) FROM Offering WHERE date_for_stay<=%s AND date_for_stay>=%s AND listing_id=l.id)) AND %s::MONEY>=(((%s-guests_included)*extra_people*%s)::MONEY + (SELECT SUM(price) FROM Offering WHERE date_for_stay<=%s AND date_for_stay>=%s AND listing_id=l.id)) AND %s>=minimum_nights AND %s<=maximum_nights AND %s<=bedrooms AND %s<=beds", [neighborhood, numGuests, numGuests, nights, checkout, checkin, minPrice, numGuests, nights, checkout, checkin, maxPrice, numGuests, nights, checkout, checkin, nights, nights, numRooms, numBeds])
-	matches=list(matches)
-	context = {'diversity': diversity, 'cuisine': cuisine, 'restaurant':restaurant, 'foodPrice':foodPrice, 'diet':diet, 'listingurl':matches[0].listing_url}
+	matches=list(matches)	
 
-	# return render(request, 'recommender/simpleMatches.html', context)
+	maxscore=0
+	toplisting=''
+	for i in range(len(matches)):
+		allbusinesses= Business.objects.raw("SELECT * FROM Business WHERE 2 * 3961 * asin( sqrt((sin( radians(  (%s - Business.latitude) / 2 ))) ^ 2  + cos(radians(%s)) * cos(radians(Business.latitude)) * (sin( radians((%s - Business.longitude) / 2) ) ) ^ 2) )   < 5", [matches[i].latitude, matches[i].latitude, matches[i].longitude])
+		
+		allbusinesses=list(allbusinesses)
+		businesscount= len(allbusinesses)
+		
+		divset = set()
+		cuisinecount=0;
+		restaurantcount=0;
+		pricecount=0;
+		dietcount=0;
+		for business in allbusinesses:
+			divset.add(business.category)
+			if business.category==cuisine:
+				cuisinecount+=1
+			if business.category==restaurant:
+				restaurantcount+=1
+			if business.price==foodPrice:
+				pricecount+=1
+			if business.category==diet:
+				dietcount+=1
+		diversitycount = len(divset)
+		
+		wc=0
+		wr=0
+		wd=0
+		wdv=0
+		wp=0
+
+		if p1=='cuisine':
+		 	wc=.3
+		if p2=='cuisine':
+		 	wc=.25	
+		if p3=='cuisine':
+		 	wc=.2
+		if p4=='cuisine':
+		 	wc=.15
+		if p5=='cuisine':
+		 	wc=.1
+
+		if p1=='restaurant':
+		 	wr=.3
+		if p2=='restaurant':
+		 	wr=.25	
+		if p3=='restaurant':
+		 	wr=.2
+		if p4=='restaurant':
+		 	wr=.15
+		if p5=='restaurant':
+		 	wr=.1
+
+		if p1=='diet':
+		 	wd=.3
+		if p2=='diet':
+		 	wd=.25	
+		if p3=='diet':
+		 	wd=.2
+		if p4=='diet':
+		 	wd=.15
+		if p5=='diet':
+		 	wd=.1
+
+		if p1=='diversity':
+		 	wdv=.3
+		if p2=='diversity':
+		 	wdv=.25	
+		if p3=='diversity':
+		 	wdv=.2
+		if p4=='diversity':
+		 	wdv=.15
+		if p5=='diversity':
+		 	wdv=.1
+
+
+		if p1=='price':
+		 	wp=.3
+		if p2=='price':
+		 	wp=.25	
+		if p3=='price':
+		 	wp=.2
+		if p4=='price':
+		 	wp=.15
+		if p5=='price':
+		 	wp=.1
+
+		score= wc* cuisinecount/businesscount + wr*restaurantcount/businesscount+ wdv*diversitycount/businesscount+wd*dietcount/businesscount+ wp*pricecount/businesscount
+		if score>=maxscore:
+			maxscore=score
+			toplisting=matches[i].listing_url
+
+	context = {'diversity': diversity, 'cuisine': cuisine, 'restaurant':restaurant, 'foodPrice':foodPrice, 'diet':diet, 'listingurl':toplisting}
 	return render(request, 'recommender/listingDisplayPage.html', context)
-
-def simple(request):
-	top_listings=Listing.objects.order_by('-id')[:5] #we can change this to order by match_weight later	
-	context = {'top_listings': top_listings}
-	return render(request, 'recommender/simple.html', context) 
-
-"""
-For each listing in matches, get all businesses within distance.
-diversity = SELECT COUNT(*) FROM view
-cuisine = 
-"""
